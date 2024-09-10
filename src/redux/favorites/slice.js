57275fc8058929e9f46ae43310a21ca938819228
Olexandrd <olexandrd@gmail.com>
2024-09-10 23:27:21 +0300

    feat: Add favorites slice for managing favorite items

---
+++ src/redux/favorites/slice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const favoritesSlice = createSlice({
+  name: "favorites",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addFavorite: (state, action) =>
+      (state.items = state.items.push(action.payload)),
+    removeFavorite: (state, action) =>
+      (state.items = state.items.filter((item) => item.id !== action.payload)),
+  },
+});
+
+export const { addFavorite, removeFavorite } = favoritesSlice.actions;
+export const favoritesReducer = favoritesSlice.reducer;
