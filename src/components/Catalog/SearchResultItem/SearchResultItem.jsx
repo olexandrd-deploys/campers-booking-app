69de2195e62d5eaffa74e3666169cd86f28dfd90
Olexandrd <olexandrd@gmail.com>
2024-09-12 18:31:33 +0300

    item card style

--- src/components/Catalog/SearchResultItem/SearchResultItem.jsx
+++ src/components/Catalog/SearchResultItem/SearchResultItem.jsx
@@ -24,48 +24,50 @@ const SearchResultItem = ({
         />
       </div>
       <div className={css.itemInfo}>
-        <div className={css.itemHeaderContainer}>
-          <div className={css.itemHeader}>
-            <h2>{name}</h2>
-            <div className={css.priceContainer}>
-              <p className={css.price}>{`€${price}.00`}</p>
-              <button className={css.favoritesButton}>
-                <svg className={css.icon}>
-                  <use href="/icons.svg#favorites"></use>
+        <div className={css.itemInfoWrapper}>
+          <div className={css.itemHeaderContainer}>
+            <div className={css.itemHeader}>
+              <h2>{name}</h2>
+              <div className={css.priceContainer}>
+                <p className={css.price}>{`€${price}.00`}</p>
+                <button className={css.favoritesButton}>
+                  <svg className={css.icon}>
+                    <use href="/icons.svg#favorites"></use>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className={css.itemSubHeader}>
+              <p className={css.rating}>
+                <svg>
+                  <use href="/icons.svg#rating"></use>
                 </svg>
-              </button>
+                {rating}({reviews.length} Reviews)
+              </p>
+              <p className={css.location}>
+                <svg>
+                  <use href="/icons.svg#map"></use>
+                </svg>
+                {location.split(", ").reverse().join(", ")}
+              </p>
             </div>
           </div>
-          <div className={css.itemSubHeader}>
-            <p className={css.rating}>
-              <svg>
-                <use href="/icons.svg#rating"></use>
-              </svg>
-              {rating}({reviews.length} Reviews)
-            </p>
-            <p className={css.location}>
-              <svg>
-                <use href="/icons.svg#map"></use>
-              </svg>
-              {location.split(", ").reverse().join(", ")}
-            </p>
+          <p className={css.description}>{description}</p>
+          <div className={css.itemFeatures}>
+            {featuredList
+              .filter((feature) => camper[feature.name])
+              .map((feature) => (
+                <div key={feature.name} className={css.feature}>
+                  <svg className={css.icon}>
+                    <use href={`/icons.svg#${feature.icon}`}></use>
+                  </svg>
+                  <p>
+                    {feature.name.charAt(0).toUpperCase() +
+                      feature.name.slice(1)}
+                  </p>
+                </div>
+              ))}
           </div>
-        </div>
-        <p className={css.description}>{description}</p>
-        <div className={css.itemFeatures}>
-          {/* <ItemFeatures item={camper} /> */}
-          {featuredList
-            .filter((feature) => camper[feature.name])
-            .map((feature) => (
-              <div key={feature.name} className={css.feature}>
-                <svg className={css.icon}>
-                  <use href={`/icons.svg#${feature.icon}`}></use>
-                </svg>
-                <p>
-                  {feature.name.charAt(0).toUpperCase() + feature.name.slice(1)}
-                </p>
-              </div>
-            ))}
         </div>
         <NavLink to={`/catalog/${camper.id}`}>
           <Button>Show more</Button>
